7108eb04d5ec6659343ac0efd3158ded1f3df82a
MenagoChicken <lewandowski.michau@gmail.com>
2019-05-15 18:34:02 +0200

    init commit

---
+++ app/src/main/java/pl/menagochicken/NotesListActivity.java
@@ -0,0 +1,13 @@
+package pl.menagochicken;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notes_list_activity);
+    }
+}
