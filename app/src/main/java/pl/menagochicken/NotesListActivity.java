26775aa5deeab20235e50b3adabc1bdcf98df840
MenagoChicken <lewandowski.michau@gmail.com>
2019-05-15 19:09:28 +0200

    data model

--- app/src/main/java/pl/menagochicken/NotesListActivity.java
+++ app/src/main/java/pl/menagochicken/NotesListActivity.java
@@ -1,13 +1,16 @@
 package pl.menagochicken;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class NotesListActivity extends AppCompatActivity {
+
+    private static final String TAG = "NotesListActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notes_list_activity);
+
     }
 }
