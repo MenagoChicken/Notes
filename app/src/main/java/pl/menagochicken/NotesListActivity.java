410f9a51c50b94ce7dc3f985bcd48cff33415582
MenagoChicken <lewandowski.michau@gmail.com>
2019-05-30 19:38:47 +0200

    Setting Up a RecyclerView

--- app/src/main/java/pl/menagochicken/NotesListActivity.java
+++ app/src/main/java/pl/menagochicken/NotesListActivity.java
@@ -2,15 +2,53 @@ package pl.menagochicken;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+
+import pl.menagochicken.adapters.NotesRecyclerAdapter;
+import pl.menagochicken.models.Note;
 
 public class NotesListActivity extends AppCompatActivity {
 
     private static final String TAG = "NotesListActivity";
 
+    // Ui componnents
+    private RecyclerView mRecyclerView;
+
+    //vars
+    private ArrayList<Note> mNotes = new ArrayList<>();
+    private NotesRecyclerAdapter mNotesRecyclerAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notes_list_activity);
+        mRecyclerView = findViewById(R.id.recyclerView);
 
+        initRecyclerView();
+        insertFakeNotes();
+
+    }
+
+    private void insertFakeNotes(){
+        for (int i = 0; i < 1000 ; i++) {
+            Note note = new Note();
+            note.setTitle("title # " + i);
+            note.setContent("content # " + i);
+            note.setTimeStamp(" Jan 2019");
+            mNotes.add(note);
+        }
+        mNotesRecyclerAdapter.notifyDataSetChanged();
+    }
+
+    private void initRecyclerView(){
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mNotesRecyclerAdapter = new NotesRecyclerAdapter(mNotes);
+        mRecyclerView.setAdapter(mNotesRecyclerAdapter);
     }
 }
