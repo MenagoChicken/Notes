68c991a642d27bf9d579ca3b958c4e61d0fdb0db
MenagoChicken <lewandowski.michau@gmail.com>
2019-06-02 19:29:07 +0200

    NoteActivity Initialization

--- app/src/main/java/pl/menagochicken/NoteActivity.java
+++ app/src/main/java/pl/menagochicken/NoteActivity.java
@@ -3,10 +3,19 @@ package pl.menagochicken;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import pl.menagochicken.models.Note;
 
 public class NoteActivity extends AppCompatActivity {
+
+    // ui components
+    private EditText mEditTitle, mEditText;
+    private TextView mViewTitle;
+
+    //vars
+    private boolean mIsNewNote;
 
     private static final String TAG = "NoteActivity";
     @Override
@@ -14,13 +23,26 @@ public class NoteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
 
-        if (getIntent().hasExtra("selectedNote")) {
-            Note note = getIntent().getParcelableExtra("selectedNote");
-            Log.d(TAG, "onCreate: " + note.toString());
+        mEditText = findViewById(R.id.note_text);
+        mEditTitle = findViewById(R.id.note_edit_title);
+        mViewTitle = findViewById(R.id.note_text_title);
+
+        if (getIncomingIntent()){
+            //new note (EDIT MODE)
+        } else {
+            //not new Note (View MODE)
         }
 
-        //VIEW
+    }
 
-        //EDIT
+    private boolean getIncomingIntent(){
+        if (getIntent().hasExtra("selectedNote")){
+            Note incomingNote = getIntent().getParcelableExtra("selectedNote");
+            Log.d(TAG, "getIncomingIntent: " + incomingNote.toString());
+            mIsNewNote = false;
+            return false;
+        }
+        mIsNewNote = true;
+        return true;
     }
 }
