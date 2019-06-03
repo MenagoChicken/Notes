fb58f33db2f2f0bfe3db7dc60c29abee9fd4d757
MenagoChicken <lewandowski.michau@gmail.com>
2019-06-03 16:47:34 +0200

    OnDoubleTapListener

--- app/src/main/java/pl/menagochicken/NoteActivity.java
+++ app/src/main/java/pl/menagochicken/NoteActivity.java
@@ -3,12 +3,17 @@ package pl.menagochicken;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import pl.menagochicken.models.Note;
 
-public class NoteActivity extends AppCompatActivity {
+public class NoteActivity extends AppCompatActivity implements View.OnTouchListener,
+        GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener
+{
 
     // ui components
     private EditText mEditTitle, mEditText;
@@ -16,6 +21,8 @@ public class NoteActivity extends AppCompatActivity {
 
     //vars
     private boolean mIsNewNote;
+    private Note mInitialNote;
+    private GestureDetector mGestureDetector;
 
     private static final String TAG = "NoteActivity";
     @Override
@@ -29,20 +36,91 @@ public class NoteActivity extends AppCompatActivity {
 
         if (getIncomingIntent()){
             //new note (EDIT MODE)
+            setNewNoteProperties();
         } else {
-            //not new Note (View MODE)
+            //not new note (VIEW MODE)
+            setNoteProperties();
         }
 
+        setListeners();
+
+    }
+
+    private void setListeners(){
+        mEditText.setOnTouchListener(this);
+        mGestureDetector = new GestureDetector(this, this);
     }
 
     private boolean getIncomingIntent(){
         if (getIntent().hasExtra("selectedNote")){
-            Note incomingNote = getIntent().getParcelableExtra("selectedNote");
-            Log.d(TAG, "getIncomingIntent: " + incomingNote.toString());
+            mInitialNote = getIntent().getParcelableExtra("selectedNote");
+            Log.d(TAG, "getIncomingIntent: " + mInitialNote.toString());
             mIsNewNote = false;
             return false;
         }
         mIsNewNote = true;
         return true;
+    }
+
+    private void setNoteProperties(){
+        mViewTitle.setText(mInitialNote.getTitle());
+        mEditTitle.setText(mInitialNote.getTitle());
+        mEditText.setText(mInitialNote.getContent());
+    }
+
+    private void setNewNoteProperties(){
+        mViewTitle.setText("Note title");
+        mEditTitle.setText("Note title");
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return mGestureDetector.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        Log.d(TAG, "onDoubleTap: double tapped");
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return false;
     }
 }
