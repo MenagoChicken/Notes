578583575162ae8bd163ef85dd1166100333b911
MenagoChicken <lewandowski.michau@gmail.com>
2019-06-05 20:59:17 +0200

    Room Persistence Database and Schema

---
+++ app/src/main/java/pl/menagochicken/persistance/NoteDao.java
@@ -0,0 +1,28 @@
+package pl.menagochicken.persistance;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+import pl.menagochicken.models.Note;
+
+@Dao
+public interface NoteDao {
+
+    @Insert
+    long[] insertNotes(Note... notes);
+
+    @Query("SELECT * FROM notes")
+    LiveData<List<Note>> getNotes();
+
+    @Delete
+    int delete(Note... notes);
+
+    @Update
+    int update(Note... notes);
+}
